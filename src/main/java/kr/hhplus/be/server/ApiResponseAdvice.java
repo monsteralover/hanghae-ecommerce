50c3bbd73255b7a4ece9c53280f3c1ffc19c0c45
monsteralover <kmj99301@gmail.com>
2025-01-06 22:03:40 +0900

    [modify] swagger 사용을 위한 RestControllerAdvice hidden 처리

--- src/main/java/kr/hhplus/be/server/ApiResponseAdvice.java
+++ src/main/java/kr/hhplus/be/server/ApiResponseAdvice.java
@@ -1,15 +1,17 @@
 package kr.hhplus.be.server;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
+@Hidden
 public class ApiResponseAdvice implements ResponseBodyAdvice<ApiResponse<?>> {
 
     @Override
