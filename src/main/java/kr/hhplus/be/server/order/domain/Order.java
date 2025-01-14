8f1535cbeb9c55a05dfaeb14b63efa63509dfdab
monsteralover <kmj99301@gmail.com>
2025-01-14 23:48:51 +0900

    [refactor] order 생성 로직 order 서비스에서 도메인 안으로 이동

--- src/main/java/kr/hhplus/be/server/order/domain/Order.java
+++ src/main/java/kr/hhplus/be/server/order/domain/Order.java
@@ -1,10 +1,18 @@
 package kr.hhplus.be.server.order.domain;
 
 import jakarta.persistence.*;
+import kr.hhplus.be.server.ApiException;
+import kr.hhplus.be.server.ApiResponseCodeMessage;
 import kr.hhplus.be.server.BaseEntity;
+import kr.hhplus.be.server.order.controller.OrderRequestItems;
+import kr.hhplus.be.server.order.service.dto.OrderServiceRequest;
+import kr.hhplus.be.server.product.domain.Product;
 import lombok.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 @Entity
 @Builder(access = AccessLevel.PACKAGE)
@@ -18,28 +26,57 @@ public class Order extends BaseEntity {
     private Long id;
     private Long userId;
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "order")
     @Getter
     private List<OrderItem> orderItems;
 
+    @Getter
     private long totalAmount;
 
     private Long couponId;
 
-    public static Order create(Long userId, List<OrderItem> orderItems, long totalAmount, Long couponId) {
-        return Order.builder()
-                .userId(userId)
-                .orderItems(orderItems)
-                .couponId(couponId)
-                .totalAmount(totalAmount)
+    public static Order create(final OrderServiceRequest request, final List<Product> products) {
+        final Order order = Order.builder()
+                .userId(request.getUserId())
+                .couponId(request.getCouponId())
                 .build();
+
+        order.initializeOrderItems(request, products);
+        return order;
     }
 
-    public void setOrderItems(final List<OrderItem> orderItems) {
+    private void initializeOrderItems(final OrderServiceRequest request, final List<Product> products) {
+        final AtomicLong totalPrice = new AtomicLong(0L);
+        final Map<Long, Integer> productIdAndQuantity = getProductIdAndQuantityMap(request);
+
+        final List<OrderItem> orderItems = getOrderItems(products, this, totalPrice, productIdAndQuantity);
+        final long totalAmount = calculatePaymentAmount(totalPrice.get(), request.getDiscountAmount());
+
         this.orderItems = orderItems;
-    }
-
-    public void setTotalAmount(final long totalAmount) {
         this.totalAmount = totalAmount;
     }
+
+    private Map<Long, Integer> getProductIdAndQuantityMap(final OrderServiceRequest request) {
+        return request.getOrderItems().stream().collect(Collectors
+                .toMap(OrderRequestItems::getProductId, OrderRequestItems::getQuantity));
+    }
+
+    private List<OrderItem> getOrderItems(final List<Product> products, final Order order,
+                                          final AtomicLong totalPrice,
+                                          final Map<Long, Integer> productIdAndQuantity) {
+        return products.stream().map(product -> {
+            final OrderItem orderItem = OrderItem.create(order, product, productIdAndQuantity.get(product.getId()));
+            totalPrice.addAndGet(orderItem.getUnitPrice());
+            return orderItem;
+        }).toList();
+    }
+
+    private long calculatePaymentAmount(long totalPrice, int discountAmount) {
+        final long totalPayment = totalPrice - discountAmount;
+        if (totalPayment < 0) {
+            throw new ApiException(ApiResponseCodeMessage.INVALID_PAYMENT_AMOUNT);
+        }
+        return totalPayment;
+    }
+
 }
--- src/main/java/kr/hhplus/be/server/product/repository/ProductRepository.java
+++ src/main/java/kr/hhplus/be/server/product/repository/ProductRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Set;
 
 public interface ProductRepository {
     Page<Product> getProductsPagination(Pageable pageable);
@@ -14,4 +15,6 @@ public interface ProductRepository {
     Product getById(Long productId);
 
     Product save(Product product);
+
+    List<Product> getAllById(Set<Long> productIds);
 }
